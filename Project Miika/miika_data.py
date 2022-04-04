dc0d87331a07b1cd7ea753818047361ee9f4041a
NimBuzz01 <mniamatmarjan@gmail.com>
2022-04-04 10:27:08 +0530

    update #1

--- Project Miika/miika_data.py
+++ Project Miika/miika_data.py
@@ -1,22 +1,62 @@
-#Reading text files into variables
+# Importing libraries
+import re
+
+
+# Reading text files into variables
 lines = open('dataset/lines.txt', encoding='utf-8', errors = 'ignore').read().split('\n')
 conversation = open('dataset/conversations.txt', encoding='utf-8', errors = 'ignore').read().split('\n')
 
-#Extracting the required data from dataset
+# Extracting the required data from dataset
 convoFlow = []
 for convo in conversation:
-    convoFlow.append(convo.split(' +++$+++ ')[-1][1:-1].replace("'","").replace(",","").split())
-    
+    convoFlow.append(convo.split(' +++$+++ ')[-1][1:-1].replace("'", " ").replace(",","").split())
+
 dialog = {}
 for line in lines:
-    dialog[line.split('+++$+++')[0]] = line.split('+++$+++')[-1]
-    
-#Mapping Answers to Questions    
+    dialog[line.split(' +++$+++ ')[0]] = line.split(' +++$+++ ')[-1]
+
+# Mapping Answers to Questions 
 questions = []
 answers = []
 
-#Convo Replies(Answers) get mapped to Convos(Questions)
+# Convo Replies(Answers) get mapped to Convos(Questions)
 for convo in convoFlow:
     for i in range(len(convo) - 1):
         questions.append(dialog[convo[i]])
         answers.append(dialog[convo[i+1]])
+        
+# Sorting dialogs based on length.
+sortedQues = []
+sortedAns = []
+for i in range(len(questions)):
+    if len(questions[i]) < 13:
+        sortedQues.append(questions[i])
+        sortedAns.append(answers[i])
+
+# Formatting all punctuations
+def formatText(txt):
+    txt = txt.lower()
+    txt = re.sub(r"i'm", "i am", txt)
+    txt = re.sub(r"he's", "he is", txt)
+    txt = re.sub(r"she's", "she is", txt)
+    txt = re.sub(r"that's", "that is", txt)
+    txt = re.sub(r"what's", "what is", txt)
+    txt = re.sub(r"where's", "where is", txt)
+    txt = re.sub(r"\'ll", " will", txt)
+    txt = re.sub(r"\'ve", " have", txt)
+    txt = re.sub(r"\'re", " are", txt)
+    txt = re.sub(r"\'d", " would", txt)
+    txt = re.sub(r"won't", "will not", txt)
+    txt = re.sub(r"can't", "can not", txt)
+    txt = re.sub(r"[^\w\s]", "", txt)
+    return txt
+
+# Appending the formatted text
+formattedQues = []
+formattedAns = []
+
+for line in sortedQues:
+    formattedQues.append(formatText(line))
+    
+for line in sortedAns:
+    formattedAns.append(formatText(line))
